58fa95617c541b2ef9f4415a6aa51f59c55d552a
brainliubo <clairlb@163.com>
2019-08-20 11:20:07 +0800

    thread communication 1st commit

---
+++ typedef.h
@@ -0,0 +1,26 @@
+/** @file typedef.h
+*
+* @brief  d2d protocol type definition
+* @author   bo.liu
+* @date     2019/07/29
+* COPYRIGHT NOTICE: (c) smartlogictech.
+* All rights reserved.
+*/
+
+#ifndef INC_TYPEDEF_H_
+#define INC_TYPEDEF_H_
+
+typedef signed char		int8_t;
+typedef unsigned char	uint8_t;
+typedef signed short	int16_t;
+typedef unsigned short	uint16_t;
+typedef signed int		int32_t;
+typedef unsigned int	uint32_t;
+
+typedef int                bool;
+typedef uint32_t           frame_t;
+typedef uint32_t           sub_frame_t;
+typedef uint16_t           rnti_t;
+
+
+#endif /* INC_TYPEDEF_H_ */
